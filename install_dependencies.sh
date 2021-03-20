4e898faeb427091652523898c4438b4d22b780a1
Dmitry Melekh <melekh.dm@gmail.com>
2021-03-20 20:07:46 +0300

    threshold sample from openCV

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,4 +3,6 @@ python3 -m venv ~/py3_venvs/ds
 cd ~/py3_venvs/ds/bin
 source ./activate
 pip install opencv-contrib-python
+pip install numpy
+pip install matplotlib
 
--- split/scr.py
+++ split/scr.py
@@ -1,4 +1,23 @@
 
-import cv2
+import os
+import cv2 as cv
+from matplotlib import pyplot as plt
 
-print("OpenCV Version: {}".format(cv2.__version__))
+print("OpenCV Version: {}".format(cv.__version__))
+
+print(os.path.dirname(os.path.realpath(__file__)))
+
+working_dir = os.path.dirname(os.path.realpath(__file__))
+pict_filepath = os.path.join(working_dir, 'resources/scan_01.jpg')
+print(os.path.exists(pict_filepath))
+
+img = cv.imread(pict_filepath,0)
+
+
+ret,th1 = cv.threshold(img,230,255,cv.THRESH_BINARY)
+
+plt.imshow(th1,'gray')
+plt.xticks([])
+plt.yticks([])
+
+plt.show()

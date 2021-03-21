09c0e394a85ec8d7863ff0b05b6507e734dccd86
Dmitry Melekh <melekh.dm@gmail.com>
2021-03-21 10:57:59 +0300

    methods extracted

--- split/scr.py
+++ split/scr.py
@@ -5,64 +5,56 @@ from matplotlib import pyplot as plt
 import re
 import numpy as np
 
-print("OpenCV Version: {}".format(cv.__version__))
 
-print(os.path.dirname(os.path.realpath(__file__)))
-
-working_dir = os.path.dirname(os.path.realpath(__file__))
-pict_filepath = os.path.join(working_dir, 'resources/scan_01.jpg')
-print(os.path.exists(pict_filepath))
-
-img = cv.imread(pict_filepath)
-img = cv.copyMakeBorder(img, 50, 50, 50, 50, cv.BORDER_CONSTANT, None, (255,255,255))
-print(f'class name of imread() result: {img.__class__.__name__}')
-imgGry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-height, width = imgGry.shape
-print(f'image width: {width}')
-print(f'image height: {height}')
-
-print(width*0.1)
-
-ret, th1 = cv.threshold(imgGry, 220, 255, cv.THRESH_BINARY)
-print(f'class name of threshold() result: {th1.__class__.__name__}')
-contours, hierarchy = cv.findContours(
-    th1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# th1, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
-# print(f'contours len: {len(contours)}')
-# for i in range(5):
-#     contour_str_repr = re.sub('[\t\r\n]+', ', ', str(contours[i]))
-#     print(f'contour #{i}: {contour_str_repr}')
+def get_test_filename():
+    print("OpenCV Version: {}".format(cv.__version__))
+    print(os.path.dirname(os.path.realpath(__file__)))
+    working_dir = os.path.dirname(os.path.realpath(__file__))
+    img_filepath = os.path.join(working_dir, 'resources/scan_01.jpg')
+    return img_filepath
 
 
-# with open('split/log.txt', 'w') as f:
-#     f.writelines([''.join(str(i))+'\n' for i in th1.tolist()])
+def get_image(img_filename):
+    img = cv.imread(img_filename)
+    img = cv.copyMakeBorder(img, 50, 50, 50, 50,
+                            cv.BORDER_CONSTANT, None, (255, 255, 255))
+    return img
 
-# print(len(th1), len(th1[0]))
 
-for contour in contours:
-    bbox = cv.boundingRect(contour)
-    x, y, w, h = bbox
-    if w < width * 0.01 or h < height * 0.01:
-        continue
-    # print(f'bounding box: {bbox}')
+def get_threshold(img, threshold_value):
+    imgGry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, th1 = cv.threshold(imgGry, threshold_value, 255, cv.THRESH_BINARY)
+    return th1
 
-    approx = cv.approxPolyDP(
-        contour, 0.01 * cv.arcLength(contour, True), True)
-    if len(approx) != 4:
-        continue
-    approx_str_repr = re.sub('[\t\r\n]+', ', ', str(approx))
-    print(f'approx: {approx_str_repr}')
+def get_rectangles(threshold):
+    contours, hierarchy = cv.findContours(
+        threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    height, width = threshold.shape
+    rectangles = []
+    for contour in contours:
+        bbox = cv.boundingRect(contour)
+        x, y, w, h = bbox
+        if w < width * 0.01 or h < height * 0.01:
+            continue
+        approx = cv.approxPolyDP(
+            contour, 0.01 * cv.arcLength(contour, True), True)
+        if len(approx) != 4:
+            continue
+        rectangles.append(approx)
+    return rectangles
+
+
+test_img_filename = get_test_filename()
+img = get_image(test_img_filename)
+threshold = get_threshold(img, 220)
+rectangles = get_rectangles(threshold)
+print(f'rectangles number: {len(rectangles)}')
+for rectangle in rectangles:
     color = list(np.random.random(size=3) * 255)
-    print(f'color: {color}')
-    cv.drawContours(img, [approx], -1, color, 5)
-    # cv.drawContours(img, [approx], -1, (0, 255, 0), 5)
-    # cv.drawContours(th1, approx, -1, (255, 0, 0), 20)
-
-# cv.drawContours(th1, contours, -1, (255, 0, 0), 5)
+    cv.drawContours(img, [rectangle], -1, color, 5)
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.imshow(th1, 'gray')
+ax1.imshow(threshold, 'gray')
 ax2.imshow(img)
 plt.xticks([])
 plt.yticks([])

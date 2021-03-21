6d4204c4409d5a419eb8a1fa2f4dcfb971952b75
Dmitry Melekh <melekh.dm@gmail.com>
2021-03-21 17:34:54 +0300

    rectangle's top left point set index 0

--- split/scr.py
+++ split/scr.py
@@ -4,11 +4,11 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import re
 import numpy as np
+import math
 
 
 def get_test_filename():
     print("OpenCV Version: {}".format(cv.__version__))
-    print(os.path.dirname(os.path.realpath(__file__)))
     working_dir = os.path.dirname(os.path.realpath(__file__))
     img_filepath = os.path.join(working_dir, 'resources/scan_01.jpg')
     return img_filepath
@@ -25,6 +25,7 @@ def get_threshold(img, threshold_value):
     imgGry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ret, th1 = cv.threshold(imgGry, threshold_value, 255, cv.THRESH_BINARY)
     return th1
+
 
 def get_rectangles(threshold):
     contours, hierarchy = cv.findContours(
@@ -44,14 +45,53 @@ def get_rectangles(threshold):
     return rectangles
 
 
+def r2(pt):
+    return math.pow(pt[0][0], 2.0) + math.pow(pt[0][1], 2.0)
+
+
+def vector_length(pt2, pt1):
+    return math.sqrt(math.pow(pt2[0][0]-pt1[0][0], 2.0) + math.pow(pt2[0][1]-pt1[0][1], 2.0))
+
+
+def set_top_left_point_index0(rectangle):
+    r2_lst = []
+    for i in range(len(rectangle)):
+        r2_lst.append(r2(rectangle[i]))
+    min_i = r2_lst.index(min(r2_lst))
+    if min_i == 0:
+        return
+    for i in range(len(rectangle)-min_i):
+        buffer = np.copy(rectangle[len(rectangle)-1])
+        for j in range(len(rectangle)-1, 0, -1):
+            rectangle[j] = rectangle[j-1]
+        rectangle[0] = buffer
+
+
+def rotate_and_crop(img, rectangle):
+    basic_pt = rectangle[0]
+    basic_di = 0.5 * \
+        (vector_length(rectangle[3], rectangle[0]) +
+         vector_length(rectangle[2], rectangle[1]))
+    basic_dj = 0.5 * \
+        (vector_length(rectangle[1], rectangle[0]) +
+         vector_length(rectangle[3], rectangle[2]))
+    rectangle_str_repr = re.sub('[\t\r\n]+', ', ', str(rectangle))
+    print(
+        f'rectangle_str_repr: {rectangle_str_repr};\n    rectangle side i: {basic_di};\n    rectangle side j: {basic_dj}')
+
+
 test_img_filename = get_test_filename()
 img = get_image(test_img_filename)
 threshold = get_threshold(img, 220)
 rectangles = get_rectangles(threshold)
 print(f'rectangles number: {len(rectangles)}')
 for rectangle in rectangles:
+    set_top_left_point_index0(rectangle)
     color = list(np.random.random(size=3) * 255)
     cv.drawContours(img, [rectangle], -1, color, 5)
+    # rotate_and_crop(img, rectangle)
+    
+
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.imshow(threshold, 'gray')
